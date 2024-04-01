a468c0243dd0a1555c4fa17b96ce3c78396b41ca
matheus899 <105080795+matheus899@users.noreply.github.com>
2024-04-01 10:35:01 -0300

    m

--- main.js
+++ main.js
@@ -16,22 +16,21 @@ for (let j=0;j<botoes.length;j++){
  
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31");
-let tempoAtual = new Date();
- 
-  contadores[0].textContent = calculatempo(tempoObjetivo1);
+contadores[0].textContent = calculatempo(tempoObjetivo1);
+  
   function calculatempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos =  Math.floor(tempoFinal/1000);
+    let minutos = Math.floor (segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
 
+   segundos %= 60;
+   minutos %=60;
+   horas %=24;
+
+   return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
   }
 
  
-let tempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos =  Math.floor(tempoFinal/1000);
-let minutos = Math.floor (segundos/60);
-let horas = Math.floor (minutos/60);
-let dias = Math.floor (horas/24);
-
- segundos %= 60;
-  minutos %=60;
-   horas %=24;
-
-contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
